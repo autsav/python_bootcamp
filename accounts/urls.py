e7f1b41169f818696621e4b344f1f74f00173563
autsav <autsav73@gmail.com>
2021-08-29 16:46:38 +0545

    Django User management 2

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,4 @@
-from .views import login_view, profile_view
+from .views import login_view, profile_view,logout_view
 from django.urls import path
 # from django.views import View
 
@@ -7,4 +7,5 @@ app_name = 'accounts'
 urlpatterns = [
     path('login/', login_view, name="login"),
     path('profile/', profile_view, name="profile"),
+    path('logout/', logout_view, name="logout"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,8 @@ from accounts.forms import LoginForm
 from django.shortcuts import redirect, render
 # from accounts.forms import LoginForm
 from .forms import LoginForm
-from django.contrib.auth import authenticate,login
+from django.contrib.auth import authenticate,login, logout
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
@@ -21,14 +22,21 @@ def login_view(request):
             else:
                 print("auth credentials do not match")
     elif request.method == 'GET':
+        if request.user.is_authenticated:
+            return redirect('/accounts/profile/')
         form = LoginForm()
 
     return render(request,'accounts/login.html',{'form': form})
 
-
+@login_required(login_url='/accounts/login/')
+#incase of login_url='', it takes login_url from settings
 def profile_view(request):
-    if request.user.is_authenticated:
-        pass
-    else:
-        pass
+    # if request.user.is_authenticated:
+    #     pass
+    # else:
+    #     pass
     return render(request,'accounts/profile.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('/accounts/login/')
