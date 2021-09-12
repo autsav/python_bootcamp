92a5911df90e0af05cb64f2355a623158df1c3e2
autsav <autsav73@gmail.com>
2021-09-12 09:59:19 +0545

    pagination

--- rest/generic_views.py
+++ rest/generic_views.py
@@ -3,12 +3,17 @@ from rest_framework.generics import CreateAPIView, ListAPIView, DestroyAPIView, 
 
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.filters import SearchFilter, OrderingFilter
+
+from django_filters.rest_framework import DjangoFilterBackend
+
 from .serializers import InfoModelSerializer
 from .models import Info
+from .pagination import MyLimitOffsetPagination
 
 
 class InfoModelCreateAPIView(CreateAPIView):
-    serializer_class = InfoModelSerializer
+    
 
     def perform_create(self, serializer):
         serializer.save()
@@ -17,8 +22,14 @@ class InfoModelCreateAPIView(CreateAPIView):
         
 class InfoModelListAPIView(ListAPIView):
     serializer_class = InfoModelSerializer
-    authentication_classes = [TokenAuthentication, ]
-    permission_classes = [IsAuthenticated,]
+    pagination_class = MyLimitOffsetPagination
+    filter_backends = [SearchFilter, OrderingFilter, DjangoFilterBackend]
+
+    search_fields = ['name']
+    order_fields = ['name', 'id']
+    filterset_fields = ['name','address']
+    # authentication_classes = [TokenAuthentication, ]
+    # permission_classes = [IsAuthenticated,]
 
     def get_queryset(self):
         # return super().get_queryset()(self):
